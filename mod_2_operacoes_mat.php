c9a2feff0a51d40be1c8e83fa4368467ffe75734
Weverton Matias <weverton-matias@hotmail.com>
2022-10-26 17:25:35 -0300

    Trabalhando com operações matemáticas.

---
+++ mod_2_operacoes_mat.php
@@ -0,0 +1,15 @@
+<?php
+    $idade = 27;
+    $idadeDaqui10anos = $idade + 10;
+    $idadeDivididade2 = $idade/2;
+
+    echo $idadeDaqui10anos . "<br>";
+    echo $idadeDivididade2 . "<br>";
+
+    $doisAoCubo = 2 ** 3;
+
+    echo $doisAoCubo . "<br>";
+
+    $restoDivisao = 3%2;
+    
+    echo $restoDivisao;
