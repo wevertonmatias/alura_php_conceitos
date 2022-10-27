1dd5083d8f6a45c6ad70df37994dbe6c8d5f7525
Weverton Matias <weverton-matias@hotmail.com>
2022-10-27 12:58:04 -0300

    Entendendo string.

---
+++ mod_3_string.php
@@ -0,0 +1,12 @@
+<?php
+
+$idade = 22;
+
+echo "Olá Mundo" . PHP_EOL;
+
+echo 'Minha idade é $idade' . PHP_EOL;
+echo 'Minha idade é ' . $idade . PHP_EOL;
+echo "Minha idade é $idade" . PHP_EOL;
+echo "Minha idade é {$idade}" . PHP_EOL;
+$new_message = "Minha idade é {$idade}";
+echo $new_message . PHP_EOL;
