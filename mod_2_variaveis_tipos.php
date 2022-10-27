ce47cf2cb7f68163db416df8920af17c4bd9f329
Weverton Matias <weverton-matias@hotmail.com>
2022-10-27 10:00:02 -0300

    Entendendo os tipos primitivos de variáveis.

---
+++ mod_2_variaveis_tipos.php
@@ -0,0 +1,21 @@
+<?php
+
+$idade = 21;
+
+echo "{$idade} ".gettype($idade). PHP_EOL;
+
+$salario = 1500.55;
+
+echo "{$salario} " . gettype($salario) . PHP_EOL;
+
+$divisao = 10 / 3;
+
+echo "{$divisao} " . gettype($divisao) . PHP_EOL;
+
+$condicao = true;
+
+echo "{$condicao} " . gettype($condicao) . PHP_EOL;
+
+$mensagem = "Olá Mundo!";
+
+echo "{$mensagem} " . gettype($mensagem) . PHP_EOL;
