4171ac23f8b1a5c2458d767ed4ac31d691019255
Weverton Matias <weverton-matias@hotmail.com>
2022-10-28 10:34:20 -0300

    Trabalhando com operadores lógicos.

---
+++ mod_4_oper_log.php
@@ -0,0 +1,12 @@
+<?php
+
+$idade = 18;
+$nome = "weverton";
+
+echo "Você tem {$idade} anos" . PHP_EOL;
+
+//if($idade == 18 || $idade > 18){
+//if($idade >= 18 && $nome == "weverton"){
+if($idade >= 18){
+    echo "Pode entrar" . PHP_EOL;
+}
