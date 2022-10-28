9a00fdfc48b80d4306072dad8558a9dd9cb140d1
Weverton Matias <weverton-matias@hotmail.com>
2022-10-28 11:59:10 -0300

    Condicionais, else.

--- mod_4_oper_log.php
+++ mod_4_oper_log.php
@@ -9,4 +9,6 @@ echo "Você tem {$idade} anos" . PHP_EOL;
 //if($idade >= 18 && $nome == "weverton"){
 if($idade >= 18){
     echo "Pode entrar" . PHP_EOL;
+}else{
+    echo "Você não pode entrar";
 }
